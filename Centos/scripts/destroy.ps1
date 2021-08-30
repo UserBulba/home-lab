ca3443987a565b2e2d02a0dbd725385219626b7d
UserBulba <foxxxiu@gmail.com>
2021-08-30 13:54:08 +0200

    Update destroy.ps1

--- Centos/scripts/destroy.ps1
+++ Centos/scripts/destroy.ps1
@@ -5,9 +5,12 @@ Push-Location -Path ..\
 
 Write-Host "`Stopping vagrant..." -ForegroundColor Yellow
 Invoke-Expression -Command "vagrant halt"
+
 Write-Host "`nDestroying vagrant..." -ForegroundColor Yellow
 Invoke-Expression -Command "vagrant destroy --force"
+
 Write-Host "`nStopping sync job..." -ForegroundColor Yellow
-Get-Job | Stop-Job | Remove-Job
+Get-Job | Stop-Job 
+Get-Job | Remove-Job
 
 Pop-Location
