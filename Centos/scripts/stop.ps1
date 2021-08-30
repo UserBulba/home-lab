3c18364de281ea176d820cef445ffd75e8a2aff8
UserBulba <foxxxiu@gmail.com>
2021-08-30 13:57:36 +0200

    Create stop.ps1

---
+++ Centos/scripts/stop.ps1
@@ -0,0 +1,13 @@
+### Stop local machine.
+
+# Stop vagrant.
+Push-Location -Path ..\
+
+Write-Host "`Stopping vagrant..." -ForegroundColor Yellow
+Invoke-Expression -Command "vagrant halt"
+
+Write-Host "`nStopping sync job..." -ForegroundColor Yellow
+Get-Job | Stop-Job 
+Get-Job | Remove-Job
+
+Pop-Location
